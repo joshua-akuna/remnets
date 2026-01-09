0736e2ef499ba115bcd47017262bec0a46c94408
Akuna Joshua <akunajoshua@gmail.com>
2026-01-09 16:09:01 +0100

    Header and Post components created for App.jsx page

---
+++ components/Post.jsx
@@ -0,0 +1,27 @@
+export default function Post() {
+  return (
+    <div className='post'>
+      <div className='image'>
+        <img
+          src='https://images.unsplash.com/photo-1761839259946-2d80f8e72e18?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          alt=''
+        />
+      </div>
+      <div className='texts'>
+        <h2>Full house battery backup coming later this year</h2>
+        <p className='info'>
+          <a href='' className='author'>
+            Akuna Joshua
+          </a>
+          <time>2024-06-10 10:00</time>
+        </p>
+        <p className='summary'>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab quos
+          maiores praesentium voluptate labore sequi, quis doloribus rem
+          officiis vero minima ea debitis vitae repudiandae. Ipsa temporibus
+          architecto repudiandae ratione.
+        </p>
+      </div>
+    </div>
+  );
+}
