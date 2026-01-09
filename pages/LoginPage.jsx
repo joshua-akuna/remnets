5106854318d8b10c2fd268c4db1a52cad3434b74
Akuna Joshua <akunajoshua@gmail.com>
2026-01-09 22:02:23 +0100

    LoginPage implemented

--- pages/LoginPage.jsx
+++ pages/LoginPage.jsx
@@ -1,9 +1,42 @@
+import { useState } from 'react';
+
 export default function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function loginUser(ev) {
+    ev.preventDefault();
+
+    const url = 'http://localhost:4000/api/v1/login';
+    await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
   return (
-    <form className='form'>
+    <form className='form' onSubmit={loginUser}>
       <h1>Login</h1>
-      <input type='text' placeholder='Username' />
-      <input type='password' placeholder='Password' />
+      <input
+        type='text'
+        placeholder='Username'
+        value={username}
+        onChange={(ev) => setUsername(ev.target.value)}
+      />
+      <input
+        type='password'
+        placeholder='Password'
+        value={password}
+        onChange={(ev) => setPassword(ev.target.value)}
+      />
       <button>Login</button>
     </form>
   );
