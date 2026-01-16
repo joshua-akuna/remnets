228a39a81e09bbf81275dd05851bc73dac68ec16
Akuna Joshua <akunajoshua@gmail.com>
2026-01-16 11:04:50 +0100

    implemented createPost page using react-quill-new

--- pages/CreatePost.jsx
+++ pages/CreatePost.jsx
@@ -1,3 +1,107 @@
+import { useState } from 'react';
+import ReactQuill from 'react-quill-new';
+import 'react-quill-new/dist/quill.snow.css';
+
+// Define custom toolbar modules
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }],
+    [{ size: ['small', false, 'large', 'huge'] }],
+    ['bold', 'italic', 'underline', 'strike'],
+    [{ color: [] }, { background: [] }],
+    [{ script: 'sub' }, { script: 'super' }],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    [{ indent: '-1' }, { indent: '+1' }],
+    [{ direction: 'rtl' }],
+    [{ align: [] }],
+    ['link', 'image', 'video'],
+    ['blockquote', 'code-block'],
+    ['clean'], // remove formatting
+  ],
+  clipboard: {
+    matchVisual: false, // prevents pasting with formatting
+  },
+};
+
+// Define which formats are allowed
+const formats = [
+  'header',
+  'font',
+  'size',
+  'bold',
+  'italic',
+  'underline',
+  'strike',
+  'color',
+  'background',
+  'script',
+  'list',
+  // 'bullet',
+  'indent',
+  'direction',
+  'align',
+  'link',
+  'image',
+  'video',
+  'blockquote',
+  'code-block',
+];
+
 export default function CreatePost() {
-  return <h1>Create New Posts</h1>;
+  // states for inputs and ReactQuill
+  const [title, setTitle] = useState('');
+  const [summary, setSummary] = useState('');
+  const [content, setContent] = useState('');
+  const [files, setFiles] = useState(null);
+
+  async function createPost(ev) {
+    ev.preventDefault();
+
+    const formData = new FormData();
+    formData.set('title', title);
+    formData.set('summary', summary);
+    formData.set('content', content);
+    formData.set('file', files[0]);
+
+    const url = 'http://localhost:4000/api/v1/post';
+    const response = await fetch(url, {
+      method: 'POST',
+      credentials: 'include',
+      body: formData,
+    });
+    console.log(await response.json());
+  }
+
+  return (
+    <form onSubmit={createPost}>
+      <input
+        type='text'
+        placeholder='Title'
+        value={title}
+        onChange={(ev) => setTitle(ev.target.value)}
+      />
+      <input
+        type='text'
+        placeholder='Summary'
+        value={summary}
+        onChange={(ev) => setSummary(ev.target.value)}
+      />
+      <input
+        type='file'
+        name='file'
+        id='file'
+        onChange={(ev) => setFiles(ev.target.files)}
+      />
+      <ReactQuill
+        theme='snow'
+        modules={modules}
+        formats={formats}
+        value={content}
+        onChange={setContent}
+        placeholder='Start writing...'
+      />
+      <button>Create Post</button>
+    </form>
+  );
 }
