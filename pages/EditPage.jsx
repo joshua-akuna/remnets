1ed5a97b44b4e5e637b9e29385e9c3cc65cd24d7
Akuna Joshua <akunajoshua@gmail.com>
2026-01-20 23:56:29 +0100

    EditPage page implemented to edit posts

--- pages/EditPage.jsx
+++ pages/EditPage.jsx
@@ -1,3 +1,63 @@
+import { useState } from 'react';
+import { Navigate, useParams } from 'react-router-dom';
+import Editor from '../components/Editor';
+
 export default function EditPost() {
-  return <div>Edit Post</div>;
+  const [title, setTitle] = useState('');
+  const [summary, setSummary] = useState('');
+  const [files, setFiles] = useState(null);
+  const [content, setContent] = useState('');
+  const [redirect, setRedirect] = useState(null);
+  const { id } = useParams();
+
+  const updatePost = async (ev) => {
+    ev.preventDefault();
+
+    const formData = new FormData();
+    formData.set('title', title);
+    formData.set('summary', summary);
+    formData.set('content', content);
+    formData.set('file', files?.[0]);
+
+    const url = `http://localhost:4000/api/v1/posts/${id}`;
+    const response = await fetch(url, {
+      method: 'PUT',
+      credentials: 'include',
+      body: formData,
+    });
+    // const data = await response.json();
+    // console.log(data);
+    if (response.ok) {
+      setRedirect(true);
+    }
+  };
+
+  if (redirect) {
+    return <Navigate to={'/'} />;
+  }
+
+  return (
+    <form onSubmit={updatePost}>
+      <input
+        type='text'
+        value={title}
+        placeholder='Enter Post Title'
+        onChange={(ev) => setTitle(ev.target.value)}
+      />
+      <input
+        type='text'
+        placeholder='Enter Post Summary'
+        value={summary}
+        onChange={(ev) => setSummary(ev.target.value)}
+      />
+      <input
+        type='file'
+        name='file'
+        id='file'
+        onChange={(ev) => setFiles(ev.target.files)}
+      />
+      <Editor value={content} onChange={setContent} />
+      <button style={{ marginTop: '10px' }}>Edit Post</button>
+    </form>
+  );
 }
