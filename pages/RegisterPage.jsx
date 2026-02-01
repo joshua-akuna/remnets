ac1f79aada1103c1ca484924656e01e5ebcb83b3
Akuna Joshua <akunajoshua@gmail.com>
2026-02-01 14:23:53 +0100

    debugged RegisterPage.jsx

--- pages/RegisterPage.jsx
+++ pages/RegisterPage.jsx
@@ -1,26 +1,38 @@
 import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
 
 export default function RegisterPage() {
   // register form states
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [redirect, setRedirect] = useState(false);
 
   // async function to register new user
   async function registerUser(ev) {
     // prevents form default bahaviour
     ev.preventDefault();
+    setRedirect(false);
 
     // sends a post request to the backend register endpoint
-    // const url = 'http://localhost:4000/api/v1/auth/register';
-    const url = 'https://memnet-api.vercel.app/api/v1/auth/register';
-    await fetch(url, {
-      method: 'POST',
-      body: JSON.stringify({ username, password }),
-      headers: { 'Content-Type': 'application/json' },
-    })
-      .then((response) => response.json())
-      .then((data) => console.log('Success:', data))
-      .catch((error) => console.error('Error:', error));
+    try {
+      // const url = 'http://localhost:4000/api/v1/auth/register';
+      const url = 'https://memnet-api.vercel.app/api/v1/auth/register';
+      const registerResponse = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      const registerData = await registerResponse.json();
+      console.log('Success:', registerData);
+      setRedirect(true);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+  // redirect to login page after successful registration
+  if (redirect) {
+    return <Navigate to={'/login'} />;
   }
 
   // JSX form for the register page
